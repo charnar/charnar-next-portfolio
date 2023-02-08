4823e30c329be0d3c4e7d7d20d6e55e9b7504601
charnar <charn.arunkit@gmail.com>
2023-02-08 23:05:10 +0700

    added menuitems and CSR for navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,9 +3,10 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 
 import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
+import MenuItems from "./menuitems";
 
 const Navbar = () => {
-  const navLinks = ["Works", "Blog"];
+  const navLinks = ["works", "blog"];
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
@@ -13,15 +14,24 @@ const Navbar = () => {
     setMounted(true);
   });
 
-  const renderThemeChanger = () => {
-    if (!mounted) return null;
+  const renderNavLinks = (link: string) => {
+    return (
+      <Link
+        className="hover:text-orange-500 dark:text-pearl-white cursor-pointer"
+        href={`/${link}`}
+      >
+        {link[0].toUpperCase() + link.slice(1)}
+      </Link>
+    );
+  };
 
+  const renderThemeChanger = () => {
     const currentTheme = theme === "system" ? systemTheme : theme;
 
     if (currentTheme === "dark") {
       return (
         <button
-          className="bg-yellow-100 w-10 h-10 text-xl flex justify-center items-center ml-10 rounded-xl"
+          className="bg-yellow-100 w-10 h-10 text-xl flex justify-center items-center rounded-xl"
           onClick={() => setTheme("light")}
         >
           <span>
@@ -43,37 +53,46 @@ const Navbar = () => {
     }
   };
 
-  return (
-    <nav className="flex justify-center items-center fixed bg-pearl-white/30 top-0 left-0 py-3 px-6 w-full backdrop-blur-xl dark:bg-dark-brown/30">
-      <div className="flex w-full max-w-3xl justify-between">
-        <ul className="flex items-center text-dark-brown text-lg gap-x-8 font-medium">
-          <Link
-            className="hover:text-orange-500 font-bold dark:text-pearl-white cursor-pointer"
-            href="/"
-          >
-            Charn A.
-          </Link>
+  const renderNavbar = () => {
+    if (!mounted) return null;
+    return (
+      <nav className="flex justify-center items-center fixed bg-pearl-white/30 top-0 left-0 py-3 px-6 w-full backdrop-blur-xl dark:bg-dark-brown/30">
+        <div className="flex w-full max-w-3xl justify-between">
+          <ul className="flex items-center text-dark-brown text-lg gap-x-8 font-medium">
+            <Link
+              className="hover:text-orange-500 font-bold dark:text-pearl-white cursor-pointer"
+              href="/"
+            >
+              Charn A.
+            </Link>
 
-          <div className="max-sm:hidden flex items-center gap-x-8">
-            <Link
-              className="hover:text-orange-500 dark:text-pearl-white cursor-pointer"
-              href="/works"
-            >
-              Works
-            </Link>
-            <Link
-              className="hover:text-orange-500 dark:text-pearl-white cursor-pointer"
-              href="/blog"
-            >
-              Blog
-            </Link>
+            <div className="max-sm:hidden flex items-center gap-x-8">
+              {navLinks.map((link: string) => renderNavLinks(link))}
+              {/* <Link
+                className="hover:text-orange-500 dark:text-pearl-white cursor-pointer"
+                href="/works"
+              >
+                Works
+              </Link>
+              <Link
+                className="hover:text-orange-500 dark:text-pearl-white cursor-pointer"
+                href="/blog"
+              >
+                Blog
+              </Link> */}
+            </div>
+          </ul>
+
+          <div className="flex">
+            {renderThemeChanger()}
+            <MenuItems></MenuItems>
           </div>
-        </ul>
+        </div>
+      </nav>
+    );
+  };
 
-        {renderThemeChanger()}
-      </div>
-    </nav>
-  );
+  return renderNavbar();
 };
 
 export default Navbar;
