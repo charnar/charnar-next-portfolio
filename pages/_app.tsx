7057d03143537ac9d41a933acc93485b03ee0de5
charnar <charn.arunkit@gmail.com>
2023-02-08 23:27:45 +0700

    added route transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { Merriweather } from "@next/font/google";
 import { Raleway } from "@next/font/google";
 
 import Layout from "./layout";
+import { useRouter } from "next/router";
 
 const merriweather = Merriweather({
   weight: ["300", "400", "700", "900"],
@@ -20,14 +22,40 @@ const raleway = Raleway({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <main
       className={`${merriweather.variable} ${raleway.variable} font-raleway`}
     >
       <ThemeProvider enableSystem={true} attribute="class">
         <Layout></Layout>
+
         <div className="py-10 px-8 max-w-3xl mx-auto">
-          <Component {...pageProps} />
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={router.route}
+              initial="initialState"
+              animate="animateState"
+              exit="exitState"
+              transition={{ duration: 0.2 }}
+              variants={{
+                initialState: {
+                  opacity: 0,
+                  y: 20,
+                },
+                animateState: {
+                  opacity: 1,
+                  y: 0,
+                },
+                exitState: {
+                  opacity: 0,
+                  y: 20,
+                },
+              }}
+            >
+              <Component {...pageProps} />
+            </motion.div>
+          </AnimatePresence>
         </div>
       </ThemeProvider>
     </main>
