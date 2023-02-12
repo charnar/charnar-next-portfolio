189d691def8020b502014cda2362298365e8fa2d
charnar <>
2023-02-13 00:27:51 +0700

    added personal proj.

--- components/workshowcase.tsx
+++ components/workshowcase.tsx
@@ -1,59 +1,47 @@
-import { prototype } from "events";
-import { TEMPORARY_REDIRECT_STATUS } from "next/dist/shared/lib/constants";
 import Image from "next/image";
-import { FC } from "react";
-
-const mockData = [
-  {
-    title: "Work 1",
-    link: "/work1",
-    description: "This is just a short description about the work",
-  },
-];
 
 interface WorkItemProp {
   title: string;
+  imageLink: string;
   link?: string;
   description?: string;
-  imageLink?: string;
 }
 
 const WorkItem = (prop: WorkItemProp) => {
   return (
     <div className="text-center flex flex-col ">
       <div className="relative rounded-xl overflow-hidden aspect-video">
-        {/* <Image
+        <Image
           src={prop.imageLink}
           alt="Work Image"
           fill
           style={{ objectFit: "cover" }}
-        /> */}
+        />
       </div>
 
       <h1 className="font-semibold mt-1 text-lg">{prop.title}</h1>
-      <h2 className="">{prop.description}</h2>
+      <h2 className="text-md">{prop.description}</h2>
     </div>
   );
 };
 
-const WorkShowcase: FC = () => {
+const WorkShowcase = () => {
   {
     const tempData = [
       {
         title: "CinnaTel",
         imageLink: "/images/cinnatel.png",
-        description: "A one-line description about work",
+        description: "A Hotel DMBS",
+      },
+      {
+        title: "InfiniVac",
+        imageLink: "/images/infinivac.png",
+        description: "The only app you need to help get through the pandemic!",
       },
     ];
 
     return (
       <div className="grid grid-cols-2 gap-x-6 gap-y-10 mt-4 max-sm:grid-cols-1">
-        {/* <WorkItem
-          title="WorkItem 1 here"
-          imageLink="/images/cinnatel.png"
-          description="A one-line description about work"
-        ></WorkItem> */}
-
         {tempData.map((item) => {
           return (
             <WorkItem
